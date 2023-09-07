da8f2f9ce3c34588b5b8aa09378be16de4027d42
T1mLen6 <timleng0911@gmail.com>
2023-09-07 15:23:20 -0400

    finished q2

--- HW1.py
+++ HW1.py
@@ -24,34 +24,65 @@ p = np.asarray([[0.1],
                 [0.1],
                 [0.1]]) 
 
+
 if condition == 2:
     p = np.asarray([[0.1],
                     [0.01],
                     [0.02]]) 
+initial_p = p
 
 B = alpha*gam*np.asarray([[1/G[0,0]],
                           [1/G[1,1]],
                           [1/G[2,2]]]) 
 
-sigma_sq = np.square(np.asarray([[sig]]))
-C = np.asarray([[0.]])
-D = np.asarray([[0.]])
-
-
-si = np.dot(G, p)
+si = np.array([G[0,0]*p[0], G[1,1]*p[1], G[2,2]*p[2]])
 qi = np.array([sig**2 + G[0,1]*p[1] + G[0,2]*p[2], sig**2 + G[1,0]*p[0] + G[1,2]*p[2], sig**2 + G[2,0]*p[0] + G[2,1]*p[1]])
 Si = si/qi
 
-p_array = np.zeros(3,1)
-s_array = np.zeros(3,1)
 
-for step in range(50):
+p_array = p
+s_array = Si
+t_array = []
+target_val = np.asarray([alpha * gam])
+
+
+for step in range(15):
     p = np.dot(A, p) + B * sig**2
+    p_array = np.hstack((p_array, p))
+    si = np.array([G[0,0]*p[0], G[1,1]*p[1], G[2,2]*p[2]])
+    qi = np.array([sig**2 + G[0,1]*p[1] + G[0,2]*p[2], sig**2 + G[1,0]*p[0] + G[1,2]*p[2], sig**2 + G[2,0]*p[0] + G[2,1]*p[1]])
+    Si = si/qi
+    s_array = np.hstack((s_array, Si))
+    t_array = np.append(t_array, step)
+    target_val = np.append(target_val, alpha * gam)
+
+t_array = np.append(t_array, t_array[-1]+1)
+
 
 # plot
-plt.figure(dpi=100)
-plt.plot(t, y)
-plt.ylabel('p [m]')
-plt.xlabel('t [s]')
+fig, axs = plt.subplots(2)
+#plt.figure(dpi=100)
+axs[0].plot(t_array, p_array[0,:], label = "p1")
+axs[0].plot(t_array, p_array[1,:], label = "p2")
+axs[0].plot(t_array, p_array[2,:], label = "p3")
+axs[0].set_title('Transmitter Power vs. Time (p = '+str(initial_p.reshape(1,3)) + ", gamma = " + str(gam) + ")")
+axs[0].set_xlabel('Time step (s)')
+axs[0].set_ylabel('Transmitter Power')
+axs[0].grid()
+axs[0].legend()
+
+axs[1].plot(t_array, s_array[0,:], label = "s1")
+axs[1].plot(t_array, s_array[1,:], label = "s2")
+axs[1].plot(t_array, s_array[2,:], label = "s3")
+axs[1].plot(t_array, target_val, label = "target", color = "red", linestyle = '--')
+axs[1].set_title('SNIR vs. Time (p = '+str(initial_p.reshape(1,3)) + ", gamma = " + str(gam) + ")")
+axs[1].set_xlabel('Time step (s)')
+axs[1].set_ylabel('SNIR')
+axs[1].grid()
+axs[1].legend()
+
+fig.tight_layout()
+
+#plt.legend()
 plt.show()
 
