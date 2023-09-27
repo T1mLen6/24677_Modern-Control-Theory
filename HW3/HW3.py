1cb4e4a03f2735a2c825b7a2ad541e8c424ef329
Letian Leng <timleng0911@gmail.com>
2023-09-27 03:05:44 -0400

    finished

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -3,29 +3,57 @@ from scipy import signal
 import matplotlib.pyplot as plt
 from numpy.linalg import svd
 import cv2
+from PIL import Image
 
-def compress_svd(image, K):
-    U,s,V = svd(image,full_matrices=False)
-    reconst_M = np.dot(U[:,:K], np.dot(np.diag(s[:K]), V[:K,:]))
+
+# def compress_svd(image, K):
+#     U,s,V = svd(image)
+#     reconst_M = np.dot(np.dot(U[:,:K], np.diag(s[:K])), V[:K,:])
     
-    return reconst_M
+#     return reconst_M, s
 
 def compress_gray_img(image, ratio):
     m = image.shape[0]
     n = image.shape[1]
-    K = (ratio/100.0) * ((m*n)/(1+m+n))
-    reconst_image = compress_svd(image, K)
-    return reconst_image
+    K = round((ratio/100) * ((m*n)/(1+m+n)))
+    print(K,m,n)
+    U,s,V = svd(image)
+    reconst_M = np.dot(np.dot(U[:,:K], np.diag(s[:K])), V[:K,:])
+    return reconst_M
 
 img = cv2.imread('CMU_Grayscale.png', 0) 
+#img = Image.open('CMU_Grayscale.png')
+#img = img.convert('LA')
 ratio = 50
 final_img = compress_gray_img(img, ratio)
+#plt.imshow(final_img, cmap='gray')
 
+
+for i in range(img.shape[0]):
+    for j in range(img.shape[1]):
+        if final_img[i,j] <= 0:
+            final_img[i,j] = 0
+            
+        if final_img[i,j] >= 255:
+            final_img[i,j] = 255
+            
+            
+    
 # Display the image
+final_img = final_img.astype(np.uint8)
 cv2.imshow("Image", final_img)
+#plt.imshow(final_img, plt.cm.gray_r)
  
-# Wait for the user to press a key
-cv2.waitKey(0)
- 
-# Close all windows
+while True:
+    # Check if any key is pressed
+    key = cv2.waitKey(1) & 0xFF
+    
+    # If the "q" key is pressed or the window is closed, break the loop
+    if key == ord("q") or key == 27:  # "q" or Esc key
+        break
+
+
+cv2.waitKey()
+
+# close all open windows
 cv2.destroyAllWindows()
